48bc0941eeef4f5bb41e6097714095b98fc398f2
Forrest Smith <forrestsmith@Forrests-MacBook-Pro.local>
2019-07-26 14:27:45 -0700

    initial commit after setting up MERN

---
+++ models/user.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+   name: {type: String, required: true},
+   college: {type: String, required: true},
+   major: {type: String, required: true},
+   classYear: {type: Number, required: true},
+   classes: [String],
+}, {
+   timestamps: true
+});
+
+module.exports = mongoose.model('User', userSchema);
