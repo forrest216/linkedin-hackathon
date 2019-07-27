da946c707a0afb9b3c9479f41371fb87cffa0ae4
Akhil Neti <akhilnneti@gmail.com>
2019-07-26 18:12:33 -0700

    Setup App.js and create Form component

---
+++ src/components/FilterStudentForm/FilterStudentForm.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class FilterStudentForm extends Component {
+	isFormInvalid() {
+		return false;
+	}
+
+	render() {
+		return (
+			<div>
+				A form will go here!
+			</div>
+		);
+	}
+}
+
+export default FilterStudentForm;
